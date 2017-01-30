22f6463338fcfff12e203c5600f384858504200d
Rodrigo Argüello <rarguellof91@gmail.com>
2017-01-30 18:50:24 +0100

    Add minimalistic api version

---
+++ api/server.py
@@ -0,0 +1,23 @@
+from flask import Flask
+
+import json
+
+app = Flask(__name__)
+
+
+@app.route('/products')
+def products():
+    with open('products.json') as data_file:
+        data = json.load(data_file)
+
+    return json.dumps(data)
+
+
+@app.route('/pay')
+def pay():
+    """Receives a cart object, validates it and returns the total price."""
+    return 'Pay page'
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8000)
